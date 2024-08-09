2d0f35309751d47931c4d93c33bf6601659c22b6
strangervn <jameschucks2002@gmail.com>
2024-08-10 04:12:38 +0700

    update dark & light mode

--- trello-web/src/App.jsx
+++ trello-web/src/App.jsx
@@ -1,15 +1,84 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Button from '@mui/material/Button'
+import {useColorScheme} from '@mui/material/styles'
+import  Typography  from '@mui/material/Typography'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
+function ModeSelect() {
+  
+  const { mode, setMode } = useColorScheme();
 
+  const handleChange = (event: SelectChangeEvent) => {
+    const selectedMode=event.target.value
+    setMode(selectedMode)
+    // setAge(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode"><Mode></Mode></InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        
+        <MenuItem value="light">
+          <div style={{display:'flex',alignItems:'center',gap:'8px'}}>
+          <LightModeIcon fontSize='small'/> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+        <Box sx={{display:'flex', alignItems:'center', gap:1}}>
+        <DarkModeIcon fontSize='small'/> Dark
+        </Box>
+        </MenuItem>
+        <MenuItem value="system">
+        <Box  sx={{display:'flex', alignItems:'center', gap:1}}>
+        <SettingsBrightnessIcon/> System
+        </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Button variant="contained">Hello world</Button>;
+      <ModeSelect/>
+      <hr />
+      <ModeToggle/>
+      <hr/>
+      <div>Van Ngoc</div>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   )
 }
--- trello-web/src/main.jsx
+++ trello-web/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <CssVarsProvider  theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider >
   </React.StrictMode>,
 )
