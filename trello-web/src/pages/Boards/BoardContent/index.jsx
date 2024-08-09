8b6005d763fb06a6a3b4558640e9833bb5dbbe45
strangervn <jameschucks2002@gmail.com>
2024-08-10 05:39:49 +0700

    relative & absolute import

---
+++ trello-web/src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,19 @@
+import ModeSelect from '../../components/ModeSelect'
+import { AppBar } from '@mui/material'
+import Box from '@mui/material/Box'
+
+function BoardContent() {
+  return (
+    <Box sx={{
+        backgroundColor:'primary.main',
+        width:'100%',
+        height:(theme)=> `calc(100vh - ${theme.trelloCustom.appBarHeight}-${theme.trelloCustom.boardBarHeight})`,
+        display:'flex',
+        alignItems:'center'
+     }}>
+       Board Content
+     </Box>
+  )
+}
+
+export default BoardContent
